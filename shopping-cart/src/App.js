2122acd9601f976cb4f1f94d2c131e9386b21535
siseventeen <176776401@qq.com>
2019-09-30 23:25:27 -0500

    Add Css

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
+  
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -12,10 +13,22 @@ const App = () => {
     fetchProducts();
   }, []);
 
-  return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+
+  return(
+    <div className = "container">
+    <h1>T-shirt Shopping Cart</h1>
+    <hr/>
+    
+    <div className = "row">
+      <div className = "col-md-8">
+      <ul>
+      {products.map(product => <li key={product.sku}> {product.title} </li>)}
+      </ul>
+      </div>
+      <div className = "col-md-4">
+      </div>
+    </div>
+    </div>
   );
 };
 
